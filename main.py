e62effa4cf87f906103f221cd82fd248ac3cadef
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2024-02-25 23:55:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os
 import yt_dlp as youtube_dl
 from youtubesearchpython import VideosSearch
 
@@ -15,33 +16,39 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'opusq',
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'opus',
+        'preferredquality': '192',
+    }],
+    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Сохраняем в папке downloads с оригинальным именем файла
+    'verbose': True,
+    'noplaylist': True,
+    'no-cache-dir': True,
+}
 
-                'preferredquality': '192',
-            }],
-            'verbose': True,
-            'noplaylist': True,
-            'no-cache-dir': True,
-        }
-
-async def play_one_song(ctx, link):
+async def download_and_play_song(ctx, link):
     try:
-        print('ityt')
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            print('tyt')
-            info = ydl.extract_info(link, download=False)
-            print('info', info)
+            info = ydl.extract_info(link, download=True)
             url2 = info['url']
-        print('info',info['url'])
-        print('url2',url2)
-        voice_channel = await ctx.author.voice.channel.connect()
-        voice_channel.play(discord.FFmpegPCMAudio(url2), after=lambda e: print('done', e))
+            # print('info:', info)
+            # print('url2:', url2)
+            voice_channel = await ctx.author.voice.channel.connect()
+            voice_channel.play(discord.FFmpegPCMAudio(fr'downloads\{info['title']}.opus'),
+                               after=lambda e: print('done', e))
+
+
+
+
         while voice_channel.is_playing():
             await asyncio.sleep(10)
+
         await voice_channel.disconnect()
+
+        # Удаляем скачанный файл после проигрывания
+        # os.remove(f'downloads/{info["title"]}.opus')
 
     except youtube_dl.DownloadError as e:
         print(f'Ошибка при скачивании контента: {e}')
@@ -49,7 +56,6 @@ async def play_one_song(ctx, link):
         print(f'Исключение клиента Discord: {e}')
     except Exception as e:
         print(f'Произошла ошибка: {e}')
-
 
 @bot.event
 async def on_ready():
@@ -62,9 +68,8 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-
 @bot.command()
 async def play(ctx, url):
-    await play_one_song(ctx,url)
-bot.run(config.token)
+    await download_and_play_song(ctx, url)
 
+bot.run(config.token)
